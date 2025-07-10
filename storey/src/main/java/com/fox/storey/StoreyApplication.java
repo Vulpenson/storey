99246b2fbd68588fbc9f81e075a3a5c2a4dcb31a
vulpenson <andreivulpe2603@gmail.com>
2025-07-10 13:38:12 +0300

    Initial commit

---
+++ storey/src/main/java/com/fox/storey/StoreyApplication.java
@@ -0,0 +1,13 @@
+package com.fox.storey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreyApplication.class, args);
+	}
+
+}
