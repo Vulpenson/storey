1b8fb7f45971d372c5719cc95f3d1ba78101db7f
vulpenson <andreivulpe2603@gmail.com>
2025-07-12 13:26:43 +0300

    Fixed infinite nesting

--- storey/src/main/java/com/fox/storey/entity/Category.java
+++ storey/src/main/java/com/fox/storey/entity/Category.java
@@ -1,13 +1,19 @@
 package com.fox.storey.entity;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import net.minidev.json.annotate.JsonIgnore;
 
 import java.util.List;
 
 @Table(name = "categories")
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Category {
 
     @Id
@@ -18,5 +24,6 @@ public class Category {
     private String name;
 
     @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JsonManagedReference
     private List<Product> products;
 }
